d77ee73e1d07d19233949b5755e515e76a488783
Tjoosten <Tjoosten3@gmail.com>
2015-02-18 02:40:24 +0100

    Added a couple of routes

--- public/index.php
+++ public/index.php
@@ -1,15 +1,41 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+  require __DIR__.'/../vendor/autoload.php';
 
-$app = new Slim\Slim();
+  $app = new Slim\Slim();
 
-$app->get('/', function() {
-    $user = new User;
-    $user->username = "Test User";
-    // $user->save();
+  $app->get('/', function() use($app) {
+    $variables = array(
+      'title' => 'Home',
+    );
 
-    echo "Hello, $user->username!";
-});
+    $app->render('/header.tpl.php', $variables);
+    $app->render('/navbar.tpl.php', $variables);
+    $app->render('/home.tpl.php');
+    $app->render('/footer.tpl.php');
+  });
 
-$app->run();
+  $app->get('/informal', function() use($app) {
+    $variables = array(
+      'title' => 'Informal'
+    );
+
+    $app->render('/header.tpl.php', $variables);
+    $app->render('/navbar.tpl.php', $variables);
+    $app->render('/results.tpl.php', $variables);
+    $app->render('/footer.tpl.php');
+  });
+
+  $app->get('/results', function() use($app) {
+    $variables = array(
+      'title' => 'Search results'
+    );
+
+    $app->render('/header.tpl.php', $variables);
+    $app->render('/navbar.tpl.php', $variables);
+    $app->render('/results.tpl.php', $variables);
+    $app->render('/footer.tpl.php');
+  });
+
+  // Bootstrap the app
+  $app->run();
